064b228cfde74c5565b39954a6d258a7bb887b56
Douglas DUTEIL <douglasduteil@gmail.com>
2022-12-21 00:25:17 +0100

    Day 2 2/2✅

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -9,11 +9,11 @@ fn main() {
 //
 
 pub fn part_one(input: &str) -> Option<u32> {
-    total_score_if_one_trust_strategy_guide(input)
+    total_score_if_one_trust_the_strategy_guide(input)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    total_score_if_one_understand_the_strategy_guide(input)
 }
 
 //
@@ -81,19 +81,53 @@ impl FromStr for YourHand {
 //
 //
 
-pub fn total_score_if_one_trust_strategy_guide(input: &str) -> Option<u32> {
-    input
-        .lines()
-        .map(|round| {
-            let (opponent_letter, your_letter) = round.split_once(' ').unwrap();
-            let YourHand::Hand(your_hand) = YourHand::from_str(your_letter).unwrap();
-            let OpponentHand::Hand(opponent_hand) =
-                OpponentHand::from_str(opponent_letter).unwrap();
-            let hand_score = score_from_hand(&your_hand);
-            let round_score = score_from_matchups((&opponent_hand, &your_hand));
-            Some((hand_score as u32) + (round_score as u32))
-        })
-        .sum()
+enum Strategy {
+    RoundResult(RoundResult),
+}
+
+impl FromStr for Strategy {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(Strategy::RoundResult(RoundResult::Lost)),
+            "Y" => Ok(Strategy::RoundResult(RoundResult::Draw)),
+            "Z" => Ok(Strategy::RoundResult(RoundResult::Won)),
+            _ => Err(()),
+        }
+    }
+}
+
+//
+//
+//
+
+pub fn total_score_if_one_trust_the_strategy_guide(input: &str) -> Option<u32> {
+    input.lines().map(guested_strategy).sum()
+}
+
+fn guested_strategy(round: &str) -> Option<u32> {
+    let (opponent_letter, your_letter) = round.split_once(' ')?;
+    let YourHand::Hand(your_hand) = your_letter.parse().unwrap();
+    let OpponentHand::Hand(opponent_hand) = opponent_letter.parse().unwrap();
+    let hand_score = score_from_hand(&your_hand);
+    let round_score = score_from_matchups(&opponent_hand, &your_hand);
+    Some((hand_score as u32) + (round_score as u32))
+}
+
+//
+
+pub fn total_score_if_one_understand_the_strategy_guide(input: &str) -> Option<u32> {
+    input.lines().map(real_strategy).sum()
+}
+
+fn real_strategy(round: &str) -> Option<u32> {
+    let (opponent_letter, strategy_letter) = round.split_once(' ')?;
+    let Strategy::RoundResult(round_result) = strategy_letter.parse().unwrap();
+    let OpponentHand::Hand(opponent_hand) = opponent_letter.parse().unwrap();
+    let YourHand::Hand(your_hand) = strategic_hand(&opponent_hand, &round_result);
+    let hand_score = score_from_hand(&your_hand);
+    let round_score = score_from_matchups(&opponent_hand, &your_hand);
+    Some((hand_score as u32) + (round_score as u32))
 }
 
 //
@@ -108,7 +142,7 @@ fn score_from_hand(hand: &Hand) -> usize {
     }
 }
 
-fn score_from_matchups((opponent_hand, your_hand): (&Hand, &Hand)) -> RoundResult {
+fn score_from_matchups(opponent_hand: &Hand, your_hand: &Hand) -> RoundResult {
     match (opponent_hand, your_hand) {
         (Hand::Paper, Hand::Rock) => RoundResult::Lost,
         (Hand::Paper, Hand::Scissors) => RoundResult::Won,
@@ -120,6 +154,21 @@ fn score_from_matchups((opponent_hand, your_hand): (&Hand, &Hand)) -> RoundResul
     }
 }
 
+fn strategic_hand(opponent_hand: &Hand, round: &RoundResult) -> YourHand {
+    match (opponent_hand, round) {
+        (Hand::Paper, RoundResult::Draw) => YourHand::Hand(Hand::Paper),
+        (Hand::Paper, RoundResult::Lost) => YourHand::Hand(Hand::Rock),
+        (Hand::Paper, RoundResult::Won) => YourHand::Hand(Hand::Scissors),
+        //
+        (Hand::Rock, RoundResult::Draw) => YourHand::Hand(Hand::Rock),
+        (Hand::Rock, RoundResult::Lost) => YourHand::Hand(Hand::Scissors),
+        (Hand::Rock, RoundResult::Won) => YourHand::Hand(Hand::Paper),
+        //
+        (Hand::Scissors, RoundResult::Draw) => YourHand::Hand(Hand::Scissors),
+        (Hand::Scissors, RoundResult::Lost) => YourHand::Hand(Hand::Paper),
+        (Hand::Scissors, RoundResult::Won) => YourHand::Hand(Hand::Rock),
+    }
+}
 //
 //
 //
@@ -131,39 +180,39 @@ mod tests {
     #[test]
     fn test_score_from_matchups() {
         assert_eq!(
-            score_from_matchups((&Hand::Rock, &Hand::Rock)),
+            score_from_matchups(&Hand::Rock, &Hand::Rock),
             RoundResult::Draw
         );
         assert_eq!(
-            score_from_matchups((&Hand::Rock, &Hand::Paper)),
+            score_from_matchups(&Hand::Rock, &Hand::Paper),
             RoundResult::Won
         );
         assert_eq!(
-            score_from_matchups((&Hand::Rock, &Hand::Scissors)),
+            score_from_matchups(&Hand::Rock, &Hand::Scissors),
             RoundResult::Lost
         );
         assert_eq!(
-            score_from_matchups((&Hand::Paper, &Hand::Rock)),
+            score_from_matchups(&Hand::Paper, &Hand::Rock),
             RoundResult::Lost
         );
         assert_eq!(
-            score_from_matchups((&Hand::Paper, &Hand::Paper)),
+            score_from_matchups(&Hand::Paper, &Hand::Paper),
             RoundResult::Draw
         );
         assert_eq!(
-            score_from_matchups((&Hand::Paper, &Hand::Scissors)),
+            score_from_matchups(&Hand::Paper, &Hand::Scissors),
             RoundResult::Won
         );
         assert_eq!(
-            score_from_matchups((&Hand::Scissors, &Hand::Rock)),
+            score_from_matchups(&Hand::Scissors, &Hand::Rock),
             RoundResult::Won
         );
         assert_eq!(
-            score_from_matchups((&Hand::Scissors, &Hand::Paper)),
+            score_from_matchups(&Hand::Scissors, &Hand::Paper),
             RoundResult::Lost
         );
         assert_eq!(
-            score_from_matchups((&Hand::Scissors, &Hand::Scissors)),
+            score_from_matchups(&Hand::Scissors, &Hand::Scissors),
             RoundResult::Draw
         );
     }
@@ -171,7 +220,10 @@ mod tests {
     #[test]
     fn test_total_score_if_one_trust_strategy_guide() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(total_score_if_one_trust_strategy_guide(&input), Some(15));
+        assert_eq!(
+            total_score_if_one_trust_the_strategy_guide(&input),
+            Some(15)
+        );
     }
 
     //
@@ -181,6 +233,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
