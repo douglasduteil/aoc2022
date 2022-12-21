4ac843f703f3b2f2c8099ee992950aff83e51561
Douglas DUTEIL <douglasduteil@gmail.com>
2022-12-21 02:35:50 +0100

    Day 3 1/2✅

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -7,11 +7,44 @@ fn main() {
 //
 
 pub fn part_one(input: &str) -> Option<u32> {
+    // Find the item type that appears in both compartments of each rucksack.
+    // What is the sum of the priorities of those item types?
+    sum_of_the_item_type_that_appears_in_both_compartments_of_each_rucksack(input)
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+//
+//
+//
+
+// Find the item type that appears in both compartments of each rucksack.
+pub fn sum_of_the_item_type_that_appears_in_both_compartments_of_each_rucksack(
+    input: &str,
+) -> Option<u32> {
+    use itertools::Itertools;
+    use std::collections::HashMap;
+
+    let letter_scores = ('a'..='z')
+        .chain('A'..='Z')
+        .enumerate()
+        .map(|(i, c)| (c, i + 1))
+        .collect::<HashMap<char, usize>>();
+
+    input
+        .lines()
+        .flat_map(|rucksack| {
+            let (left, right) = rucksack.split_at(rucksack.len() / 2);
+            left.chars()
+                .filter(|item| right.contains(*item))
+                .unique()
+                .collect_vec()
+        })
+        .map(|item| letter_scores.get(&item).unwrap())
+        .map(|priority| Some(*priority as u32))
+        .sum()
 }
 
 //
@@ -25,10 +58,11 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 3);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(157));
     }
 
     #[test]
+    #[ignore]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 3);
         assert_eq!(part_two(&input), None);
