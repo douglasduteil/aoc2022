cea07f2cfe30cad7b1ef419a1cbefc75171add14
Douglas DUTEIL <douglasduteil@gmail.com>
2023-01-04 20:23:08 +0100

    refactor(2022/04): host ranges in struct

--- 2022/day_04/src/lib.rs
+++ 2022/day_04/src/lib.rs
@@ -1,6 +1,6 @@
 //
 
-use std::convert::Infallible;
+use std::{convert::Infallible, ops::RangeInclusive};
 
 pub fn part_one(input: &str) -> Option<u32> {
     Some(
@@ -18,17 +18,29 @@ pub fn part_two(_input: &str) -> Option<u32> {
 
 //
 
-struct ElfPair {
-    first: std::ops::RangeInclusive<usize>,
-    second: std::ops::RangeInclusive<usize>,
+struct Elf(RangeInclusive<usize>);
+
+impl std::str::FromStr for Elf {
+    type Err = Infallible;
+
+    fn from_str(section: &str) -> Result<Self, Self::Err> {
+        let (section_start, section_end) = section
+            .split_once('-')
+            .expect("invalid section assignments : {section_1}");
+        let start = section_start.parse().expect("invalid section id");
+        let end = section_end.parse().expect("invalid section id");
+        Ok(Elf(std::ops::RangeInclusive::new(start, end)))
+    }
 }
+struct ElfPair(Elf, Elf);
 
 impl ElfPair {
     fn is_fully_overlapping(&self) -> bool {
-        let first_contains_second =
-            self.first.contains(self.second.start()) && self.first.contains(self.second.end());
-        let second_contains_first =
-            self.second.contains(self.first.start()) && self.second.contains(self.first.end());
+        let (first, second) = (&self.0, &self.1);
+        let first = &first.0;
+        let second = &second.0;
+        let first_contains_second = first.contains(second.start()) && first.contains(second.end());
+        let second_contains_first = second.contains(first.start()) && second.contains(first.end());
         first_contains_second || second_contains_first
     }
 }
@@ -37,20 +49,11 @@ impl std::str::FromStr for ElfPair {
     type Err = Infallible;
 
     fn from_str(section_pair: &str) -> Result<Self, Self::Err> {
-        let assignments_to_range = |section: &str| {
-            let (section_start, section_end) = section
-                .split_once('-')
-                .expect("invalid section assignments : {section_1}");
-            let start = section_start.parse().expect("invalid section id");
-            let end = section_end.parse().expect("invalid section id");
-            std::ops::RangeInclusive::new(start, end)
-        };
-
         let (section_1, section_2) = section_pair
             .split_once(',')
             .expect("invalid section assignments pair : {section_pair}");
-        let first = assignments_to_range(section_1);
-        let second = assignments_to_range(section_2);
-        Ok(ElfPair { first, second })
+        let first = section_1.parse().expect("invalid elf");
+        let second = section_2.parse().expect("invalid elf");
+        Ok(ElfPair(first, second))
     }
 }
