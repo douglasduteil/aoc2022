52109ff7d2956d0799f3e7c5859de287a6ede53d
Douglas DUTEIL <douglasduteil@gmail.com>
2023-01-07 01:58:18 +0100

    Day 6 1/2✅

--- 2022/day_06/src/lib.rs
+++ 2022/day_06/src/lib.rs
@@ -1,9 +1,19 @@
 //
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+use std::collections::HashSet;
+
+//
+
+pub fn part_one(input: &str) -> Option<usize> {
+    input
+        .chars()
+        .collect::<Vec<_>>()
+        .as_slice()
+        .windows(4)
+        .position(|datastream| HashSet::<_>::from_iter(datastream).len() == 4)
+        .and_then(|start| Some(start + 4))
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<usize> {
     None
 }
--- 2022/day_06/src/main.rs
+++ 2022/day_06/src/main.rs
@@ -10,14 +10,64 @@ mod tests {
     use puzzles::{part_one, part_two};
 
     #[test]
-    fn test_part_one() {
+    fn test_part_one_example1() {
         let input = advent_of_code::read_example();
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input.lines().nth(0).unwrap()), Some(7));
     }
 
     #[test]
-    fn test_part_two() {
+    fn test_part_one_example2() {
         let input = advent_of_code::read_example();
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_one(&input.lines().nth(1).unwrap()), Some(5));
+    }
+
+    #[test]
+    fn test_part_one_example3() {
+        let input = advent_of_code::read_example();
+        assert_eq!(part_one(&input.lines().nth(2).unwrap()), Some(6));
+    }
+
+    #[test]
+    fn test_part_one_example4() {
+        let input = advent_of_code::read_example();
+        assert_eq!(part_one(&input.lines().nth(3).unwrap()), Some(10));
+    }
+
+    #[test]
+    fn test_part_one_example5() {
+        let input = advent_of_code::read_example();
+        assert_eq!(part_one(&input.lines().nth(4).unwrap()), Some(11));
+    }
+
+    //
+
+    #[test]
+    fn test_part_two_example1() {
+        let input = advent_of_code::read_example();
+        assert_eq!(part_two(&input.lines().nth(0).unwrap()), None);
+    }
+
+    #[test]
+    fn test_part_two_example2() {
+        let input = advent_of_code::read_example();
+        assert_eq!(part_two(&input.lines().nth(1).unwrap()), None);
+    }
+
+    #[test]
+    fn test_part_two_example3() {
+        let input = advent_of_code::read_example();
+        assert_eq!(part_two(&input.lines().nth(2).unwrap()), None);
+    }
+
+    #[test]
+    fn test_part_two_example4() {
+        let input = advent_of_code::read_example();
+        assert_eq!(part_two(&input.lines().nth(3).unwrap()), None);
+    }
+
+    #[test]
+    fn test_part_two_example5() {
+        let input = advent_of_code::read_example();
+        assert_eq!(part_two(&input.lines().nth(4).unwrap()), None);
     }
 }
